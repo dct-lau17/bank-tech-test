39a8729ba7996f4c1662b2b061295dbbeb707960
Daniel Lau <dct.lau17@gmail.com>
2018-08-13 23:24:43 +0100

    Add deposit method to increase account balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,9 +1,20 @@
 require 'account'
 RSpec.describe Account do
-  describe 'defaults' do
-    subject(:account) { described_class.new }
+  subject(:account) { described_class.new }
+
+  describe '#defaults' do
     it 'has a balance of 0' do
       expect(account.balance).to eq 0
+    end
+  end
+
+  describe '#deposit' do
+    before do
+      account.deposit(100)
+    end
+
+    it 'increases the bank balance by the specified amount' do
+      expect(account.balance).to eq 100
     end
   end
 end
