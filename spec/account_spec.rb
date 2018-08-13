d4716428a0339b7ff212ee102fcf33050befbb39
Daniel Lau <dct.lau17@gmail.com>
2018-08-13 23:07:29 +0100

    Add default balance in account class

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,9 @@
 require 'account'
+RSpec.describe Account do
+  describe 'defaults' do
+    subject(:account) { described_class.new }
+    it 'has a balance of 0' do
+      expect(account.balance).to eq 0
+    end
+  end
+end
