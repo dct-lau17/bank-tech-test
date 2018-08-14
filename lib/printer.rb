d24e3c1358c89944fefb4816e694fc2cd6157aa7
Daniel Lau <dct.lau17@gmail.com>
2018-08-14 14:27:22 +0100

    Add format method to print out transaction in printer class

--- lib/printer.rb
+++ lib/printer.rb
@@ -5,11 +5,23 @@ class Printer
 
   def print_statement
     puts header
+    puts @transactions.reverse.map { |transaction|
+      format(transaction)
+    }.join("\n")
   end
 
   private
 
   def header
     %w[date credit debit balance].join(' || ')
+  end
+
+  def format(transaction)
+    [
+      transaction.date,
+      transaction.credit,
+      transaction.debit,
+      transaction.balance
+    ].join(' || ')
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,16 +2,28 @@ require 'printer'
 
 RSpec.describe Printer do
   describe '#print_statement' do
-
     before(:each) do
-      transaction1 = double(:transaction, credit: 100, debit: nil, date: '11/11/2010', balance: 100)
-      transaction2 = double(:transaction, credit: nil, debit: 50, date: '12/11/2010', balance: 50)
+      transaction1 = double(:transaction,
+                            credit: 100,
+                            debit: nil,
+                            date: '11/11/2010',
+                            balance: 100)
+      transaction2 = double(:transaction,
+                            credit: nil,
+                            debit: 50,
+                            date: '12/11/2010',
+                            balance: 50)
       log = [transaction1, transaction2]
       @printer =  described_class.new(log)
     end
 
-    it 'includes a header' do
-      expect(STDOUT).to receive(:puts).with('date || credit || debit || balance')
+    it 'prints full statementin reverse order' do
+      expected_header = 'date || credit || debit || balance'
+      expected_output_trans1 = '11/11/2010 || 100 ||  || 100'
+      expected_output_trans2 = '12/11/2010 ||  || 50 || 50'
+      expected_output = [expected_output_trans2, expected_output_trans1].join("\n")
+      expect(STDOUT).to receive(:puts).with(expected_output)
+      expect(STDOUT).to receive(:puts).with(expected_header)
       @printer.print_statement
     end
   end
